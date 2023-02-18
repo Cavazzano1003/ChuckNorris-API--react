b8b476625a45020bbf247802a8b5b5e28319fd43
roman0f <tamuliunaite.andrius@panettipitagora.edu.it>
2023-02-18 11:58:58 +0100

    inizio lavori

---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './styles/App.css'
+
+function Title() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="Title">
+    <title></title>
+    </div>
+  )
+}
+
+export default title
